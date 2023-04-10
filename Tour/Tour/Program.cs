330c14842f96f7c669a9ef2ba46791ff6843f41b
FilippoRizzato <94640711+FilippoRizzato@users.noreply.github.com>
2023-04-10 17:47:56 +0200

    Commit

---
+++ Tour/Tour/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            string desti;
+            string codice;
+            Console.WriteLine("Inserisci il codice");
+            codice = Console.ReadLine();
+            TourOperator tourOperator = new TourOperator(codice);
+            Console.WriteLine("Inserire un nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("Inserire un nome e una località");
+            desti = Console.ReadLine();
+
+            tourOperator.add(nome, desti);
+           
+
+            Console.WriteLine("Numero di clienti: " + tourOperator.size());
+
+            var client = (Tuple<string, string>)tourOperator.find(codice);
+            Console.WriteLine("Cliente:"+ codice +  client.Item1 + ", " + client.Item2);
+
+                     
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tour/Tour/TourOperator.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface CD
+{
+    void insert(IComparable key, object attribute);
+    object find(IComparable key);
+    object remove(IComparable key);
+}
+
+public interface Container
+{
+    bool isEmpty();
+    void makeEmpty();
+    int size();
+}
+
+public class TourOperator : CD, Container
+{
+    private Dictionary<string, Tuple<string, string>> clients;
+    private string nextClientCode;
+
+    public TourOperator(string initialClientCode)
+    {
+        clients = new Dictionary<string, Tuple<string, string>>();
+        nextClientCode = initialClientCode;
+    }
+
+    public void insert(IComparable key, object attribute)
+    {
+        if (attribute is Tuple<string, string> clientInfo)
+        {
+            clients[(string)key] = clientInfo;
+        }
+    }
+
+    public object find(IComparable key)
+    {
+        if (clients.TryGetValue((string)key, out var value))
+        {
+            return value;
+        }
+        else
+        {
+            throw new KeyNotFoundException();
+        }
+    }
+
+    public object remove(IComparable key)
+    {
+        if (clients.TryGetValue((string)key, out var value))
+        {
+            clients.Remove((string)key);
+            return value;
+        }
+        else
+        {
+            throw new KeyNotFoundException();
+        }
+    }
+
+    public void add(string nome, string dest)
+    {
+        insert(nextClientCode, new Tuple<string, string>(nome, dest));
+        nextClientCode = GenerateNextClientCode(nextClientCode);
+    }
+
+    private string GenerateNextClientCode(string currentCode)
+    {
+        int numericPart = int.Parse(currentCode.Substring(1));
+        char letterPart = currentCode[0];
+        if (numericPart == 999)
+        {
+            numericPart = 0;
+            letterPart++;
+        }
+        else
+        {
+            numericPart++;
+        }
+        return $"{letterPart}{numericPart:D3}";
+    }
+
+    public bool isEmpty()
+    {
+        return clients.Count == 0;
+    }
+
+    public void makeEmpty()
+    {
+        clients.Clear();
+    }
+
+    public int size()
+    {
+        return clients.Count;
+    }
+}
